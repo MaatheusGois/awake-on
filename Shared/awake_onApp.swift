fdd08f046a55d599c25138251684d4e5166f905c
Gois <matheusgoislimasilva@gmail.com>
2021-11-08 22:47:13 -0300

    Initial Commit

---
+++ Shared/awake_onApp.swift
@@ -0,0 +1,17 @@
+//
+//  awake_onApp.swift
+//  Shared
+//
+//  Created by Matheus Gois on 08/11/21.
+//
+
+import SwiftUI
+
+@main
+struct awake_onApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
